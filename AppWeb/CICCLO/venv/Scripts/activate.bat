58411f26bb380965c499c3f8515ecab57a590633
Jhonatan Laiton <65196460+JhonatanLa@users.noreply.github.com>
2024-08-23 16:33:11 -0500

    lib

--- AppWeb/CICCLO/venv/Scripts/activate.bat
+++ AppWeb/CICCLO/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jhona\Documents\GitHub\Proyectos\AppWeb\CICCLO\venv
+set VIRTUAL_ENV=C:\Users\jhona\Documents\GitHub\Proyectos\Proyectos\AppWeb\CICCLO\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
